958fb408e5609b8d514ca0878edfb7e9190c468b
Kevin Malakoff <kmalakoff@gmail.com>
2020-05-04 21:58:03 -0700

    Use legacy-extends

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
+var extend = require('legacy-extends');
+
 var Dirent = require('./lib/Dirent');
 var constants = require('./lib/constants');
 
-module.exports = (function () {
-  var kStats = typeof Symbol !== 'undefined' ? Symbol('stats') : 'stats';
+var kStats = typeof Symbol !== 'undefined' ? Symbol('stats') : 'stats';
 
-  var _super = require('./lib/createHelpers').createSuper(Dirent);
-  function DirentFromStats(name, stats) {
-    var _this = _super.call(this);
-    _this.name = name;
-    _this[kStats] = stats;
-    return _this;
-  }
-  DirentFromStats.prototype = Object.create(Dirent.prototype);
-  DirentFromStats.prototype.constructor = DirentFromStats;
+function DirentFromStats(name, stats) {
+  var self = DirentFromStats.__constructor__.call(this, name);
+  self[kStats] = stats;
+  return self;
+}
+extend(DirentFromStats, Dirent, ['name']);
 
-  DirentFromStats.DirentBase = Dirent;
-  DirentFromStats.constants = constants;
+DirentFromStats.prototype = Object.create(Dirent.prototype);
+DirentFromStats.prototype.constructor = DirentFromStats;
 
-  DirentFromStats.prototype.isDirectory = function isDirectory() {
-    return this[kStats].isDirectory();
-  };
-  DirentFromStats.prototype.isFile = function isFile() {
-    return this[kStats].isFile();
-  };
-  DirentFromStats.prototype.isBlockDevice = function isBlockDevice() {
-    return this[kStats].isBlockDevice();
-  };
-  DirentFromStats.prototype.isCharacterDevice = function isCharacterDevice() {
-    return this[kStats].isCharacterDevice();
-  };
-  DirentFromStats.prototype.isSymbolicLink = function isSymbolicLink() {
-    return this[kStats].isSymbolicLink();
-  };
-  DirentFromStats.prototype.isFIFO = function isFIFO() {
-    return this[kStats].isFIFO();
-  };
-  DirentFromStats.prototype.isSocket = function isSocket() {
-    return this[kStats].isSocket();
-  };
-  return DirentFromStats;
-})();
+DirentFromStats.DirentBase = Dirent;
+DirentFromStats.constants = constants;
+
+DirentFromStats.prototype.isDirectory = function isDirectory() {
+  return this[kStats].isDirectory();
+};
+DirentFromStats.prototype.isFile = function isFile() {
+  return this[kStats].isFile();
+};
+DirentFromStats.prototype.isBlockDevice = function isBlockDevice() {
+  return this[kStats].isBlockDevice();
+};
+DirentFromStats.prototype.isCharacterDevice = function isCharacterDevice() {
+  return this[kStats].isCharacterDevice();
+};
+DirentFromStats.prototype.isSymbolicLink = function isSymbolicLink() {
+  return this[kStats].isSymbolicLink();
+};
+DirentFromStats.prototype.isFIFO = function isFIFO() {
+  return this[kStats].isFIFO();
+};
+DirentFromStats.prototype.isSocket = function isSocket() {
+  return this[kStats].isSocket();
+};
+
+module.exports = DirentFromStats;
